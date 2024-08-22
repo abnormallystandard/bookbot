817394da916fe69d0466c7518d45558a67df6fea
davidweng <david297946716@gmail.com>
2024-08-22 12:52:06 -0700

    polish code

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 def main():
-    count = {}
-    char = []
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        counting = file_contents.lower()
+    char_count(word_count(text()) ,count(text()))
 
+
+def text(file_contents):
+    print(file_contents)
+
+def count(file_contents):
+    count = {}
+    counting = file_contents.lower()
     for i in counting:
         count[i] = 1 + count.get(i, 0)
+    return count
     
-    def sort_on(dict):
-        return dict["num"]
+def sort_on(dict):
+    return dict["num"]
+
+def word_count(text):
+    words = text.split()
+    return len(words)
+    
+def char_count(words, count):
+    char = []
     
     for j in count:
         if not j.isalpha():
@@ -17,13 +28,15 @@ def main():
         char.append({"char": j, "num": count[j]})
     char.sort(reverse=True, key=sort_on)
 
-
-
-
     print("--- Begin report of books/frankenstein.txt ---")
-    print("77986 words found in the document\n")
+    print(f"{words} words found in the document\n")
     for k in char:
         print(f"The '{k['char']}' character was found '{k['num']}' times")
     print("--- End report ---")
+
+def text():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    return file_contents
 
 main()
